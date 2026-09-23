a0b358408e29d3146fa3c6a591dd1d859abea29b
Md. Ragib Ashhab <ragibashhab2733@gmail.com>
2026-09-23 23:27:17 +0600

    Fix Linux case-sensitive tile resource path

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -30,7 +30,7 @@ public class TileManager {
 		
 		setup(0, "grass", false);
 		setup(1, "wall", true);
-		setup(2, "Water", true);
+		setup(2, "water", true);
 		setup(3, "earth", false);
 		setup(4, "tree", true);
 		setup(5, "sand", false);
